5c0672f49ee4c15a93db2f822911c55ca199b5f0
Wynter Jones <wynter@monetizedesign.com>
2022-05-24 22:52:59 -0400

    lil edits..

--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import '../styles/globals.css'
+import '../styles/xterm.css'
 
 function OceanTerminal({ Component, pageProps }) {
   return <Component {...pageProps} />
--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,23 +1,15 @@
 import Toolbar from '../components/Toolbar'
 import Link from 'next/link'
 
-import electron from 'electron'
-import { useEffect } from 'react'
-const ipcRenderer = electron.ipcRenderer || false
-
 function Home() {
-  useEffect(() => {
-    console.log(ipcRenderer.sendSync('run-command', 'whoami'))
-  })
-
   return (
-    <>
+    <div>
       <Toolbar />
       <div className="grid grid-col-1 w-full text-center">
         <h1 className="text-2xl">Ocean::Terminal</h1>
         <Link href="/tab/0">Open Terminal</Link>
       </div>
-    </>
+    </div>
   )
 }
 
